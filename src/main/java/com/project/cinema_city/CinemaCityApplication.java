b63a4cd7067ab3ce6266fddbda47c74a5f883a87
d4ng3r228 <dangersof228@gmail.com>
2023-06-18 14:51:12 +0300

    Initial commit

---
+++ src/main/java/com/project/cinema_city/CinemaCityApplication.java
@@ -0,0 +1,13 @@
+package com.project.cinema_city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cinema_city/CinemaCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cinema_city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
